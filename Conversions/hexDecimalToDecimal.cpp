946a18b22cbff547372336d59eaa49a290d2d9dc
nerd-sourav <souravprasad1869@gmail.com>
2021-08-03 20:32:51 +0530

    Few conversions added

---
+++ Conversions/hexDecimalToDecimal.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int hexToDec(string n)
+{
+}
+
+int main()
+{
+
+    string n;
+    cin >> n;
+    cout << hexToDec(n) << endl;
+
+    return 0;
+}
