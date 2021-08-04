cbf96a6a8e9f8b7c5c6bb5681e1c89c5f4b435c5
nerd-sourav <souravprasad1869@gmail.com>
2021-08-04 19:46:50 +0530

    Nothing Much

--- Conversions/hexDecimalToDecimal.cpp
+++ Conversions/hexDecimalToDecimal.cpp
@@ -3,6 +3,22 @@
 using namespace std;
 int hexToDec(string n)
 {
+    int s = n.size();
+    int x = 1;
+    int ans = 0;
+    for (int i = s - 1; i >= 0; i--)
+    {
+        if (n[i] >= '0' && n[i] <= '9')
+        {
+            ans += x * (n[i] - '0');
+        }
+        else if (n[i] >= 'A' && n[i] <= 'F')
+        {
+            ans += x * (n[i] - 'A' + 10);
+        }
+        x = x * 16;
+    }
+    return ans;
 }
 
 int main()
