41f7daef927614d8e862bf2ff0557e0b6a5f1105
nerd-sourav <souravprasad1869@gmail.com>
2021-08-07 00:01:28 +0530

    s

--- Arrays/bubbleSort.cpp
+++ Arrays/bubbleSort.cpp
@@ -5,7 +5,7 @@ void bubbleSort(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - i; j++)
+        for (int j = 0; j < n - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
