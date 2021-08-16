8d6805b17bfd69d7f5adf0045dcdd6cc5dafea30
nerd-sourav <souravprasad1869@gmail.com>
2021-08-16 10:20:39 +0530

    problems added

---
+++ Arrays/Challenges/allPossibleSubarrays.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int st = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k];
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
