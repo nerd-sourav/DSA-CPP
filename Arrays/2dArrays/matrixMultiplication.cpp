c0092c09292881d5ef508def8220d8597ebdf656
nerd-sourav <souravprasad1869@gmail.com>
2021-08-22 13:36:37 +0530

    matrices

---
+++ Arrays/2dArrays/matrixMultiplication.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n1 = 3;
+    int n2 = 4;
+    int n3 = 3;
+    int arr1[n1][n2] = {{2, 4, 1, 2},
+                        {8, 4, 3, 6},
+                        {1, 7, 9, 5}};
+    int arr2[n2][n3] = {{1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9},
+                        {4, 5, 6}};
+
+    int ans[n1][n3];
+    for (int i = 0; i < n; i++)
+    {
+        int currentSum = 0;
+        for (int j = 0; j < n; j++)
+        {
+        }
+    }
+
+    return 0;
+}
